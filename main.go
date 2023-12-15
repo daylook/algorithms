b711e401736f57a5d151bc55dde0ed0251fbef41
daylook <mygasht@gmail.com>
2023-12-15 20:36:00 +0100

    adding some functions to main

--- main.go
+++ main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/daylook/algorithms/divideConquer"
+)
+
 func main() {
+	data := []int{70, 250, 50, 80, 140, 12, 14}
+	max := divideConquer.MinInSlice(data, 0, len(data))
+	min := divideConquer.MaxInSlice(data, 0, len(data))
+
+	fmt.Printf("Maximum number in a given slice:%d\n", max)
+	fmt.Printf("Minimum number in a given slice:%d\n", min)
 }
